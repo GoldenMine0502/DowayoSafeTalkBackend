8f9707fa823ff9e1982c17507a3a5d1d54973245
GoldenMine0502 <ehe123@naver.com>
2024-07-10 02:31:52 +0900

    revert

--- deberta_model.py
+++ deberta_model.py
@@ -71,7 +71,8 @@ class DebertaClassificationModel:
         # del model.config.id2label[1]
 
         # self.model = DebertaForSequenceClassification(model.config).to(device)
-        self.model = DebertaForSequenceClassification.from_pretrained("microsoft/deberta-base", num_labels=1).to(device)
+        num_labels = len(model.config.id2label)
+        self.model = DebertaForSequenceClassification.from_pretrained("microsoft/deberta-base", num_labels=num_labels, force_download=True).to(device)
 
         if checkpoint is not None:
             self.model = torch.load(checkpoint).to(device)
