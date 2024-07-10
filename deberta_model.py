3c03e7a874801a47d80a3fcd5430ff175fb3b6bf
GoldenMine0502 <ehe123@naver.com>
2024-07-10 12:29:23 +0900

    train

--- deberta_model.py
+++ deberta_model.py
@@ -147,7 +147,7 @@ class DebertaClassificationModel:
     def process(self, epoch=10):
         for i in range(1, epoch + 1):
             print("epoch {}/{}:".format(i, epoch))
-            # self.train()
+            self.train()
             self.validation()
 
             torch.save(self.model, f'deberta_{i}.pt')
