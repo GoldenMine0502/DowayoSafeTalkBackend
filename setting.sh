399ea9d061b66c08181199cf3a5bab08e2d823a8
GoldenMine0502 <ehe123@naver.com>
2024-08-13 01:21:20 +0900

    patch

--- setting.sh
+++ setting.sh
@@ -1,5 +1,8 @@
 python3 -m venv venv
 source venv/bin/activate
+
 pip install -r requirements.txt
 pip install sentencepiece
 pip install 'git+https://github.com/SKTBrain/KoBERT.git#egg=kobert_tokenizer&subdirectory=kobert_hf'
+
+cp config/config-example.yml config/config.yml
